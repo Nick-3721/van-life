b87c0eaee833100e00f4196429ac799d060783d9
Nicholas <Nick_3721@hotmail.com>
2024-11-18 20:54:18 +0000

    Nested routes added

---
+++ src/pages/Vans/VanDetail.jsx
@@ -0,0 +1,33 @@
+import { React, useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+
+export default function VanDetail() {
+  const [van, setVan] =useState({})
+  const params = useParams()
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+    .then(res => res.json())
+    .then(data => setVan(data.vans))
+      .catch(err => console.log(err))
+  }, [params.id])
+
+  return (
+    <div className="wrapper">
+      <div className="ven-detail-container">
+        {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                    <Link to={`/vans/${parseFloat(van.id)+1}`}>Next Van</Link>
+                    <Link to={`/vans/${parseFloat(van.id)-1}`}>Preious Van</Link>
+                </div>
+            ) : <h2>Loading...</h2>}
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Vans/Vans.jsx
@@ -0,0 +1,42 @@
+import { React, useState, useEffect } from "react"
+import { Link } from "react-router-dom" 
+
+
+export default function Home() {
+    const [vanData, setVanData] = useState([])
+
+    useEffect(() => {
+        fetch('/api/vans')
+        .then(res => res.json())
+        .then(data => setVanData(data.vans))
+        .catch(err => console.log(err))
+    }, [])
+    console.log(vanData)
+    const vanElements = vanData.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link 
+            to={`/vans/${van.id}`} 
+            aria-label={`view detail for ${van.name},
+                        priced at ${van.price} per day`}
+            >
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
+
+    return (
+        <div className="wrapper">
+            <div className="van-list-container">
+                <h1>Explore our van options</h1>
+                <div className="van-list">
+                    {vanElements}
+                </div>
+            </div>
+        </div>
+    )
+};
