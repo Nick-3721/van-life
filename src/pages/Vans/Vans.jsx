f1b38a9aa95da118aba6471f1774c8654adbc2b3
Nicholas <Nick_3721@hotmail.com>
2025-02-11 17:00:11 +0000

    vans filter added

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,19 +1,27 @@
 import { React, useState, useEffect } from "react"
-import { Link } from "react-router-dom" 
+import { Link, useSearchParams } from "react-router-dom" 
 
 
 export default function Home() {
+    const [searchParams, setSearchParams ] = useSearchParams()
     const [vanData, setVanData] = useState([])
 
+    let typeFilter = searchParams.get("type")
+    
     useEffect(() => {
         fetch('/api/vans')
         .then(res => res.json())
         .then(data => setVanData(data.vans))
         .catch(err => console.log(err))
     }, [])
-    console.log(vanData)
     
-    const vanElements = vanData.map(van => (
+    const displayVans = typeFilter
+        ? vanData.filter(van => van.type === typeFilter)
+        : vanData
+
+    console.log(displayVans)
+    
+    const vanElements = displayVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link 
             to={`/vans/${van.id}`} 
@@ -28,12 +36,19 @@ export default function Home() {
                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
             </Link>
         </div>
-    ))
+     ))
 
     return (
         <div className="wrapper">
             <div className="van-list-container">
                 <h1>Explore our van options</h1>
+                <div className="van-list-filter-buttons">
+                    <Link to="?type=simple" className="van-type simple">Simple</Link>
+                    <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
+                    <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
+                    <Link to="." className="van-type clear-filters">Clear Filter</Link>
+            
+                </div>
                 <div className="van-list">
                     {vanElements}
                 </div>
